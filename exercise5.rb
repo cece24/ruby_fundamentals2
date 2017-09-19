bd1538c51701e9fbf10c4237e890f984b314a6f0
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-19 14:47:58 -0400

    Exercise 5, define method for greeting with name backwards twice

---
+++ exercise5.rb
@@ -0,0 +1,8 @@
+def greet_backwards(name)
+  "Hello, #{name.reverse + name.reverse}! Welcome home."
+end
+
+puts(greet_backwards("Bob"))
+puts(greet_backwards("Shirley"))
+puts(greet_backwards("Sue"))
+puts(greet_backwards("Andy"))
