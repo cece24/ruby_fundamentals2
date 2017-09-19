2f5093dc6d1cfde16297cb221ae3a17e430454ad
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-19 15:06:41 -0400

    Call method on user provided temperature and print result in a sentence

--- exercise6.rb
+++ exercise6.rb
@@ -2,5 +2,9 @@ puts "Please enter a temperature in Fahrenheit:"
 user_temperature = gets.chomp.to_i
 
 def fahrenheit_to_celsius(temperature)
-  (temperature - 32) * (5 / 9)
+  (temperature - 32) * (5.0 / 9)
 end
+
+temperature_celsius = fahrenheit_to_celsius(user_temperature)
+
+puts "The temperature in Celsius is #{temperature_celsius}."
