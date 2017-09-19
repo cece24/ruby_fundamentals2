fdd1a01a4ee0e6db41f99a4c536339c8e462732b
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-19 15:43:12 -0400

    Replace user input code with 2 methods asking for inputs, call each method 3 times

--- exercise8.rb
+++ exercise8.rb
@@ -1,23 +1,27 @@
-puts "How far did person 1 run (in metres)?"
-distance1 = gets.to_f
-puts "How long (in minutes) did person 1 run take to run #{distance1} metres?"
-mins1 = gets.to_f
+def ask_distance
+  puts "How far did the person run (in metres)?"
+  distance = gets.to_f
+end
 
+def ask_minutes
+  puts "How long (in minutes) did the person run take to run those metres?"
+  mins1 = gets.to_f
+end
 
-puts "How far did person 2 run (in metres)?"
-distance2 = gets.to_f
-puts "How long (in minutes) did person 2 take to run #{distance2} metres?"
-mins2 = gets.to_f
-
-
-puts "How far did person 3 run (in metres)?"
-distance3 = gets.to_f
-puts "How long (in minutes) did person 3 take to run #{distance3} metres?"
-mins3 = gets.to_f
+distance1 = ask_distance
+mins1 = ask_minutes
+distance2 = ask_distance
+mins2 = ask_minutes
+distance3 = ask_distance
+mins3 = ask_minutes
 
 def speed(distance, minutes)
   distance / (minutes * 60)
 end
+
+speed1 = speed(distance1, mins1)
+speed2 = speed(distance2, mins2)
+speed3 = speed(distance3, mins3)
 
 if speed3 > speed2 && speed3 > speed1
   puts "Person 3 was the fastest at #{speed3} m/s"
