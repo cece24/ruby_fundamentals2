52e767c4d9e54ab048014af6f7d55eb873124f79
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-19 15:11:49 -0400

    Define method for wrap_text and call it with user input

--- exercise7.rb
+++ exercise7.rb
@@ -2,3 +2,11 @@ puts "Please provide text:"
 user_text = gets.chomp
 puts "Please provide a symbol(s):"
 user_symbol = gets.chomp
+
+def wrap_text(text, symbol)
+  symbol + text + symbol
+end
+
+wrapped_text = wrap_text(user_text, user_symbol)
+
+puts "#{wrapped_text}"
