6ec5b4db4608b23138d9fcf72f3345007a90275e
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-19 15:16:26 -0400

    Ask for 2 for user input symbols, call method 3 times to add all symbols

--- exercise7.rb
+++ exercise7.rb
@@ -2,11 +2,15 @@ puts "Please provide text:"
 user_text = gets.chomp
 puts "Please provide a symbol(s):"
 user_symbol = gets.chomp
+puts "Please provide an additional symbol(s):"
+user_symbol2 = gets.chomp
+puts "Please provide an additional symbol(s):"
+user_symbol3 = gets.chomp
 
 def wrap_text(text, symbol)
   symbol + text + symbol
 end
 
-wrapped_text = wrap_text(user_text, user_symbol)
+wrapped_text = wrap_text(wrap_text(wrap_text(user_text, user_symbol), user_symbol2), user_symbol3)
 
 puts "#{wrapped_text}"
